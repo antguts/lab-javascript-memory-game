d3ad35f5ff2939d73921c030d584b6507d00ec42
antguts <antgut035@gmail.com>
2020-06-17 16:49:51 -0400

    Finished memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,30 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards=[]
+    this.pairsClicked=0
+    this.pairsGuessed=0
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
-}
+
+  shuffleCards() {
+    this.cards.sort(function() {
+      return .5 - Math.random();
+    });
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked+=1
+    if(card1 == card2){
+      this.pairsGuessed+=1
+      return true
+    } else{
+      return false
+    }
+  }
+
+
+  isFinished() {
+    return (this.pairsGuessed==this.cards.length/2)
+  }
+}// end memoryGame class
